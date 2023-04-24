170034c9773c63197ead9396f06cb7940bd37664
warleyzee <warleyzee@hotmail.com>
2023-04-24 13:23:29 +0100

    fisrt test  Done

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,10 +1,9 @@
-import supertest from "supertest";
-
 describe('Beach forecast functional tests', () => {
-  it('should return a forecast with just a few times', async() => {
-    const { body, status } = await supertest(app).get('/forecast');
+  it('should return a forecast with just a few times', async () => {
+    const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
-    expect(body).toBe([{
+    // Make sure we use toEqual to check value not the object and array itself
+    expect(body).toEqual([{
       "time": "2020-04-26T00:00:00+00:00",
       "forecast": [{
         "lat": -33.792726,
@@ -35,7 +34,9 @@ describe('Beach forecast functional tests', () => {
         "waveDirection": 232.12,
         "waveHeight": 0.46,
         "windDirection": 310.48
-      }] }
+          },
+        ],
+      },
     ]);
   });
 });
