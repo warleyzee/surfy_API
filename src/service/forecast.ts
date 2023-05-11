346c5aeaaece0880eb681ce45b6324a3d52f73b2
warleyzee <warleyzee@hotmail.com>
2023-05-11 08:13:21 +0100

    refactoring forecast data

--- src/service/forecast.ts
+++ src/service/forecast.ts
@@ -15,6 +15,11 @@ export interface Beach {
     user: string;
 }
 
+export interface TimeForecast{
+    time:string;
+    forecast: BeachForecast[]
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
 export class Forecast {
@@ -22,7 +27,7 @@ export class Forecast {
 
     public async processForecastForBeaches(
         beaches: Beach[]
-        ): Promise<BeachForecast[]> {
+        ): Promise<TimeForecast[]> {
         const pointsWithCorrectSources: BeachForecast[] = [];
         for(const beach of beaches){
             const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -39,51 +44,22 @@ export class Forecast {
             }));
             pointsWithCorrectSources.push(...enrichedBeachData);
         }
-        return pointsWithCorrectSources;
+        return this.mapForecastByTime(pointsWithCorrectSources);
+    }
+    private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[]{
+        const forecastByTime: TimeForecast[] = [];
+        for(const point of forecast){
+            const timePoint = forecastByTime.find((f) => f.time == point.time)
+            if(timePoint){
+                timePoint.forecast.push(point);
+            } else{
+                forecastByTime.push({
+                    time: point.time,
+                    forecast:[point]
+                });
+            }
+        }
+        return forecastByTime;
     }
 }
 
-// import { StormGlass, ForecastPoint } from '@src/clients/stormGlass';
-
-// export enum BeachPosition {
-//   S = 'S',
-//   E = 'E',
-//   W = 'W',
-//   N = 'N',
-// }
-
-// export interface Beach {
-//   name: string;
-//   position: BeachPosition;
-//   lat: number;
-//   lng: number;
-//   user: string;
-// }
-
-// export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
-
-// export class Forecast {
-//   constructor(protected stormGlass = new StormGlass()) {}
-
-//   public async processForecastForBeaches(
-//     beaches: Beach[]
-//   ): Promise<BeachForecast[]> {
-//     const pointsWithCorrectSources: BeachForecast[] = [];
-//     for (const beach of beaches) {
-//       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-//       const enrichedBeachData = points.map((e) => ({
-//         ...{},
-//         ...{
-//           lat: beach.lat,
-//           lng: beach.lng,
-//           name: beach.name,
-//           position: beach.position,
-//           rating: 1, //need to be implemented
-//         },
-//         ...e,
-//       }));
-//       pointsWithCorrectSources.push(...enrichedBeachData);
-//     }
-//     return pointsWithCorrectSources;
-//   }
-// }
