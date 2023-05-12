a1c1664a2ea85a66cb25db31708b9445ac43cab3
warleyzee <warleyzee@hotmail.com>
2023-05-12 15:52:46 +0100

    create functional test to new beach module

---
+++ test/functional/beaches.test.ts
@@ -0,0 +1,15 @@
+describe('Beaches functional tests', () => {
+    describe('When creating a beach', () => {
+        it('should create a beach with success', async () => {
+            const newBeach = {
+                lat: -33792726,
+                lng: 151.289924,
+                name: 'Manly',
+                position: 'E',
+            };
+            const response = await global.testRequest.post('/beaches').send(newBeach);
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual(expect.objectContaining(newBeach));
+        });
+    });
+});
